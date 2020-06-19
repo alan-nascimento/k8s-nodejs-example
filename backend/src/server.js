8078c2d896fd6575166112232bbf466e75afb71e
Alan Nascimento <alan.silva.n@hotmail.com>
2020-06-19 15:40:50 -0300

    feat: create node api and config its dockerfile

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+require("dotenv/config");
+
+const express = require("express");
+const { Client } = require("pg");
+
+const PORT = 3000;
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => res.json({ hello: "world" }));
+
+const client = new Client({
+  user: "root",
+  host: "localhost",
+  database: "database",
+  password: "root",
+  port: 5432,
+});
+
+client.connect();
+
+client.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+
+  client.end();
+});
+
+server.listen(PORT, () =>
+  console.log(`Listening on port ${process.env.PORT || PORT}`)
+);
