5c9ec2abdcb7d98ccdbfa15e752d64ffa33450b1
Alan Nascimento <alan.silva.n@hotmail.com>
2020-06-23 18:42:38 -0300

    chore: change docker compose configs

--- src/server.js
+++ src/server.js
@@ -9,20 +9,32 @@ server.use(express.json());
 
 server.get('/', (req, res) => res.json({ hello: 'world' }));
 
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-});
+async function exec() {
+  try {
+    const client = new Client({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+    });
 
-client.connect();
+    console.warn(client);
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
+    await client.connect();
 
-  client.end();
-});
+    client.query('SELECT NOW()', (err, res) => {
+      console.log(err, res);
 
-server.listen(PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+      client.end();
+    });
+  } catch (err) {
+    console.warn('CATCH ERROR', err);
+  }
+}
+
+exec();
+
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
